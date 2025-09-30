d26e722ffddf33db0128d16c9cb0605e2a50966e
Ahnaf Alif <ahnafalf05@gmail.com>
2025-09-30 10:04:37 +0700

    p

--- Test.java
+++ Test.java
@@ -1,10 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hi, aku ahnaf alif");
-    }
-}
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hi, aku Rayyan");
+    
+
+     System.out.println("hi, aku Rayyan");
     }
 }
