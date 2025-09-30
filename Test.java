e53e45b31ef2a74be1705c404d16fba5f82fb5db
HyRay_io <hairay261@gmail.com>
2025-09-30 09:59:59 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,3 +3,8 @@ public class Test {
         System.out.println("hi, aku ahnaf alif");
     }
 }
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hi, aku Rayyan");
+    }
+}
